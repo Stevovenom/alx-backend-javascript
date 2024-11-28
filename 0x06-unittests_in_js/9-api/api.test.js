e4c0bc7ce8ac2172eb226080c07884516054d871
STEPHEN NYAGOL <120771979+Stevovenom@users.noreply.github.com>
2024-11-29 02:19:08 +0300

    Update api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,54 +1,36 @@
-/* eslint-disable jest/expect-expect */
-const assert = require('assert');
-const { request } = require('http');
+const request = require('request');
+const { expect } = require('chai');
 
-describe('index page', () => {
-  it('returns status code 200', () => new Promise((done) => {
-    request('http://localhost:7865', (res) => {
-      assert.strictEqual(res.statusCode, 200);
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
-    }).end();
-  }));
+    });
+  });
 
-  it('returns the welcome message', () => new Promise((done) => {
-    request('http://localhost:7865', (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        assert.strictEqual(data, 'Welcome to the payment system');
-        done();
-      });
-    }).end();
-  }));
-});
-
-describe('cart page', () => {
-  it('returns status code 200 when :id is a number', () => new Promise((done) => {
-    request('http://localhost:7865/cart/123', (res) => {
-      assert.strictEqual(res.statusCode, 200);
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
       done();
-    }).end();
-  }));
+    });
+  });
 
-  it('returns status code 404 when :id is not a number', () => new Promise((done) => {
-    request('http://localhost:7865/cart/abc', (res) => {
-      assert.strictEqual(res.statusCode, 404);
+  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
       done();
-    }).end();
-  }));
+    });
+  });
 
-  it('returns payment methods for cart :id', () => new Promise((done) => {
-    request('http://localhost:7865/cart/123', (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        assert.strictEqual(data, 'Payment methods for cart 123');
-        done();
-      });
-    }).end();
-  }));
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
 });
